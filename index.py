b4850986699199d74faae29be8d00b09c032dcb3
unknown <ashishuiux@gmail.com>
2018-09-12 12:00:55 -0700

    intial commit

---
+++ index.py
@@ -0,0 +1,7 @@
+name = input("Whats your name?: \n")
+place = input("Enter a place: \n")
+animal = input("Name an animal: \n").lower()
+thing = input("Whats your favorite thing?: \n").lower()
+
+print("Here's a story:")
+print(name +" went to " + place + " to find his " + animal + " that likes to eat " + thing + ".")
